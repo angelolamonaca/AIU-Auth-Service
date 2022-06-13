ab33107fa853848411e0b3887dd63474d79be1a4
Angelo Lamonaca <angelo.lamonaca@gmail.com>
2022-06-13 07:55:26 +0200

    Init project

---
+++ src/main/java/com/aiu/aiuauthservice/AiuAuthServiceApplication.java
@@ -0,0 +1,13 @@
+package com.aiu.aiuauthservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiuAuthServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AiuAuthServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aiu/aiuauthservice/AiuAuthServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aiu.aiuauthservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiuAuthServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
